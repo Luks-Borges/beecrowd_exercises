dca8eb6a8a61fee3ba06cfdef2df3844e7a175e9
Lucas Borges Oliveira <142835982+Luks-Borges@users.noreply.github.com>
2023-12-06 10:46:23 -0300

    Adicionando snake_case os métodos da classe Beecrowd1

--- classes/beecrowd1.rb
+++ classes/beecrowd1.rb
@@ -1,18 +1,18 @@
 class Beecrowd1  #Para cada método, um exercício
 
-    def helloWorld 
+    def hello_world 
         puts "Hello World!"
     end
 
     #Métodos de classe não tem o do
     #Definimos como argumento as variáveis para que então os testes modifiquem colocando um input com valores reais
-    def extremelyBasic(x, y)
+    def extremely_basic(x, y)
         result = x + y
 
         puts "X = #{result}"
     end
 
-    def areaOfACircle(n)
+    def area_of_a_circle(n)
         pi = 3.14159
         radius = n ** 2
         area = pi * radius
@@ -20,12 +20,12 @@ class Beecrowd1  #Para cada método, um exercício
         puts "A=#{sprintf("%.4f", area)}"
     end
 
-    def simpleSum(a, b)
+    def simple_sum(a, b)
         result = a + b
         puts "SOMA = #{result}"
     end
 
-    def simpleProduct(a, b)
+    def simple_product(a, b)
         result = a * b
         puts "PROD = #{result}"
     end
